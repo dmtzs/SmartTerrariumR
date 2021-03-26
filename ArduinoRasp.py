d5c70a1b236308ec9f181e5d7450f5f112e8d1f5
Diego MS <diego.mtzs00@gmail.com>
2021-03-25 18:05:30 -0600

    Update for python application to read good the input from the arduino via USB

--- ArduinoRasp.py
+++ ArduinoRasp.py
@@ -1,10 +1,13 @@
 import time, serial, os
 
-os.system("clear")
+#os.system("clear")#Para linux.
+os.system("cls")#Para windows.
 print("Iniciando comunicacion con arduino")
 
 # Abrimos la conexión con Arduino
-arduino = serial.Serial('/dev/ttyS0', 9600)#Se queda pendiente el puerto serial, necesito un adaptador
+#arduino = serial.Serial('/dev/ttyACM0', 9600)#Escucha en puerto serial linux mio.
+arduino = serial.Serial('COM4', 9600)#Para windows mio
+time.sleep(2)
 
 with arduino:
     while True:
@@ -13,7 +16,7 @@ with arduino:
             if not rawstring:
                 pass
             else:
-                """rawstring= rawstring.rstrip(b'\r\n')
+                rawstring= rawstring.rstrip(b'\r\n')
                 rawstring= str(rawstring)
                 aux= len(rawstring)
                 aux-= 1
@@ -22,9 +25,15 @@ with arduino:
                 arre= rawstring.split("\n")
                 print(rawstring)#Ya comprobe que en efecto ahora es una cadena lo que obtengo del serial con arduino
                 print(f"Arreglo: {arre}")
-                #print(type(rawstring))"""
-                print(rawstring)
+                #print(type(rawstring))
         except KeyboardInterrupt:
-            print("Saliendo del programa")
+            #os.system("clear")
+            os.system("cls")
+            print("Termino del programa por Ctrl+c presionado")
             arduino.close()
+            break
+        except Exception as e:
+            #os.system("clear")
+            os.system("cls")
+            print(f"Ocurrió el ERROR: {e}")
             break

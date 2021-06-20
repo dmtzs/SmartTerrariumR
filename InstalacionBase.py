27b9bebbbb17620b93e802f84820c3097a9056af
dmtzs <diego.mtzs00@gmail.com>
2021-06-20 11:08:07 -0500

    Update for requirements, examples of dht22 and added command lines

--- InstalacionBase.py
+++ InstalacionBase.py
@@ -13,12 +13,20 @@ def limparShell():
 
 def main():
     os.system("pip3 install -r requirements.txt")
+    os.system("sudo apt-get install python3-dev python3-pip")
+    os.system("sudo python3 -m pip install --upgrade pip setuptools wheel")
     #Va el git clone
+
+    """
+    Para la parte de dht22 con el raspberry
+    temporal: https://www.instructables.com/Raspberry-Pi-Tutorial-How-to-Use-the-DHT-22/
+    temporal2: https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/ --> se ve mejor este
+    """
 
 if __name__== "__main__":
     try:
         comandoShell= limparShell()
         os.system(comandoShell)
         main()
-    except Exception as e:
-        print(f"The following error ocurred: {e}")
+    except Exception as ex:
+        print(f"The following error ocurred: {ex}")
--- RaspDHT.py
+++ RaspDHT.py
@@ -23,3 +23,7 @@ def codigo1():
             print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
         else:
             print("Failed to retrieve data from humidity sensor")
+
+if __name__== "__main__":
+    codigo1()
+    #codigo2()

a8ef151cde7dc413edb885040c927a6d298b4b40
Diego Ms <diego.mtzs00@gmail.com>
2021-06-23 21:54:50 -0500

    Modification for arduino program

--- RaspSerial/RaspSerial.ino
+++ RaspSerial/RaspSerial.ino
@@ -2,69 +2,54 @@
 
 #define DHT_PIN 7
 #define DHTTYPE DHT22//DHT11
-const int pinBuzzer= 9;//Constant of the buzzer.
-int BandBoton= 0;//Global variables, it can be used for soing a reference to a pin or just a global variable.
-int adc_id= 0;
+int sensorFlotador= 2;//Global variables, it can be used to assign a reference to a pin or just a global variable.
 
 DHT dht(DHT_PIN, DHTTYPE);
-String cadeRecibida= "";//Para recibir la cadena desde el puerto serial.
+String cadeRecibida= "";//To receive the string from the serial port that is connected to the Raspberry.
 char inChar;
 
 void setup()
 {
   dht.begin();
   Serial.begin(9600);
-  pinMode(pinBuzzer, OUTPUT);
-  pinMode(8, INPUT);
-  cadeRecibida.reserve(30);//Tamaño reservado para la cadena, ver si así funciona o si se puede reducir.
+  pinMode(sensorFlotador, INPUT_PULLUP);//Pin in pull up mode
+  cadeRecibida.reserve(30);//Size reserved for the chain, see if this works or if it can be reduced.
 }
 
 void loop()
 {
   float h= dht.readHumidity();
   float t= dht.readTemperature();
-  int valor= analogRead(adc_id);
-  int EstadoBoton= digitalRead(8);
-  String cade= "";
+  String cade= "", estadoFlotador= "";
 
-  //El orden de los datos es: temp, hum, nivel agua y edo boton
+  //For floating water sensor
+  if(digitalRead(sensorFlotador)== HIGH)
+  {
+    estadoFlotador= "off";
+  }
+  else
+  {
+    estadoFlotador= "on";
+    //Aquí poner lo de rellenar el bebedero
+  }
+
+  //The order of the data is: temp, hum
   cade+= t;
   cade+= ",";
   cade+= h;
   cade+= ",";
-  cade+= valor;
-  cade+= ",";
-  cade+= BandBoton;
+  cade+= estadoFlotador;
 
   Serial.println(cade);
 
   if (Serial.available())
   {
-    inChar= Serial.read();//Lectura de caracteres.
-    cadeRecibida+= inChar;//Creación de la cadena recibida.
+    inChar= Serial.read();//Character reading.
+    cadeRecibida+= inChar;//Creation of the received string.
 
     Serial.println("Cadena que se formo: ");
     Serial.println(cadeRecibida);
   }
 
-  //For buzzer
-  if(EstadoBoton==HIGH)
-  {
-    BandBoton= 1;
-  }
-
-  if(valor<120 && BandBoton==0)
-  {
-    digitalWrite(pinBuzzer, HIGH);//Se enciende buzzer
-  }
-  else if(valor<120 && BandBoton==1)
-  {
-    digitalWrite(pinBuzzer, LOW);//Se apaga el buzzer
-  }
-  else if(valor>120)
-  {
-    BandBoton=0;
-    digitalWrite(pinBuzzer, LOW);//Se apaga el buzzer
-  }
-  delay(3000);//Para el sensor de temperatura y humedad, pero de igual manera afecta al de agua.
+  delay(4000);//For the temperature and humidity sensor cause the sensor needs time to measure the data.
 }

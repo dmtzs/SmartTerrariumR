ba5f19682ab926ef0f60b8268d85c859a852a9f6
Diego Ms <diego.mtzs00@gmail.com>
2021-06-27 12:54:33 -0500

    Added node instalations to the python file

--- InstalacionBase.py
+++ InstalacionBase.py
@@ -6,6 +6,9 @@ except ImportError as eImp:
 def main():
     os.system("sudo apt install python3-pip")
     os.system("pip3 install -r requirements.txt")
+    #Antes ver si se puede desde aquí instalar node y npm desde terminal
+    os.system("npm init")
+    os.system("npm install --save-dev electron")
     #Pensar si ejecutar desde aquí el programa de electron y el servidor de flask
 
 if __name__== "__main__":

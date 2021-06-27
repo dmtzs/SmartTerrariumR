d96bc03e8f470d5a83652e002fd2bd6bcf09d25e
dmtzs <diego.mtzs00@gmail.com>
2021-06-27 18:50:03 -0500

    Added again instalation script

---
+++ InstalacionBase.py
@@ -0,0 +1,18 @@
+try:
+    import os, platform, Metodos
+except ImportError as eImp:
+    print(f"The following import error ocurred: {eImp}")
+
+def main():
+    os.system("sudo apt install python3-pip")
+    os.system("pip3 install -r requirements.txt")
+    #Pensar si ejecutar desde aquí el programa de electron y el servidor de flask
+
+if __name__== "__main__":
+    try:
+        limpShell= Metodos.ColeccionMetodos()
+        comandoShell, plataforma= limpShell.limparShell()
+        os.system(comandoShell)
+        main()
+    except Exception as ex:
+        print(f"The following error ocurred: {ex}")

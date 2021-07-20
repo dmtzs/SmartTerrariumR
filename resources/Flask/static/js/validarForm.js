7953bbeaff1ccf268aed65122edbf649ba2f549c
Diego MS <diego.mtzs00@gmail.com>
2021-07-20 18:53:12 -0500

    Update for form

--- resources/Flask/static/js/validarForm.js
+++ resources/Flask/static/js/validarForm.js
@@ -1,3 +1,9 @@
 function validarFormulario() {
-    //Poner algo
+    let rangoTempResAgua= document.getElementById("TempAguaReserva").value;
+    let rangoTempTerrario= document.getElementById("TempTerrario").value;
+    let rangoHumedad= document.getElementById("Humedad").value;
+
+    if (rangoTempResAgua== null) {
+        alert("Prueba");
+    }
 }

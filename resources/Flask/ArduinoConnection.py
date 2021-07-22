918ccd3774ccbdf4e2752818be90de51bd860df0
Diego Ms <diego.mtzs00@gmail.com>
2021-07-22 08:36:37 -0500

    Update for arduino connection

--- resources/Flask/ArduinoConnection.py
+++ resources/Flask/ArduinoConnection.py
@@ -34,7 +34,7 @@ class ArduinoConnection():
                 arduino_ports = [
                     p.device
                     for p in serial.tools.list_ports.comports()
-                    if 'Arduino' in p.description  # may need tweaking to match new arduinos
+                    if "Arduino" in p.description or "Dispositivo" in p.description # may need tweaking to match new arduinos
                 ]
                 if not arduino_ports:
                     raise IOError("No Arduino found")

b2bf14230ca5cbf4e0ee4cb626054fd3b3724584
Diego Ms <diego.mtzs00@gmail.com>
2021-07-23 23:04:08 -0500

    Update for Instalacion base python file

--- InstalacionBase.py
+++ InstalacionBase.py
@@ -1,32 +1,41 @@
 try:
-    import os, platform
+    import os
+    import platform
 except ImportError as eImp:
     print(f"The following import error ocurred: {eImp}")
 
 
-def limpShell():
+def limpShellSystem():
     sistema = platform.system()
 
     if sistema == "Windows":
-        return "cls"
+        return "cls", sistema
     else:
-        return "clear"
+        return "clear", sistema
 
 
-def main():
+def main(sistema):
     #Pensar si incluir: "sudo apt install florence -y", "sudo apt install at-spi2-core -y"
-    comandos = ["sudo apt update", "sudo apt upgrade", "sudo apt install python3-pip", "pip3 install -r requirements.txt", "sudo apt install nodejs",
-                "sudo apt install npm", "npm install electron wait-port electron-alert", "sudo usermod -a -G dialout $USER"]
+    comandosLinux= ["sudo apt update", "sudo apt upgrade", "sudo apt install python3-pip", "pip3 install -r requirements.txt", "sudo apt install nodejs",
+                    "sudo apt install npm", "npm install electron wait-port electron-alert", "sudo usermod -a -G dialout $USER"]
+    
+    comandosWindows= "npm install electron wait-port electron-alert"
 
-    for comm in comandos:
-        os.system(comm)
+    if sistema== "Windows":
+        os.system(comandosWindows)
+    elif sistema== "Linux":
+        for comm in comandosLinux:
+            os.system(comm)
+    else:
+        print("Solo se puede ejecutar en linux y windows")
     # Pensar si ejecutar desde aquí el programa de electron.
 
 
 if __name__ == "__main__":
     try:
-        os.system(limpShell())
-        main()
+        comShell, sistema= limpShellSystem()
+        os.system(comShell)
+        main(sistema)
     except Exception as ex:
         print(f"The following error ocurred: {ex}")
     finally:

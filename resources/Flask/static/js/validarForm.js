ebb595f1e48d3822b85bd7d1e550e92c50c61720
Diego Ms <diego.mtzs00@gmail.com>
2021-07-25 16:53:47 -0500

    Updates for html templates

--- resources/Flask/static/js/validarForm.js
+++ resources/Flask/static/js/validarForm.js
@@ -1,15 +1,28 @@
 function validarFormulario() {
-    var rangoTempResAgua= document.getElementById("TempAguaReserva").value;
-	var rangoTempTerrario= document.getElementById("TempTerrario").value;
-	var rangoHumedad= document.getElementById("Humedad").value;
+    let rangoTempResAgua= document.getElementById("TempAguaReserva").value;
+	let rangoTempTerrario= document.getElementById("TempTerrario").value;
+	let rangoHumedad= document.getElementById("Humedad").value;
 
-    if (rangoTempResAgua < 15.55 || rangoTempResAgua > 35) {
+    let aux1= toString(rangoTempResAgua);
+    let aux2= toString(rangoTempTerrario);
+    let aux3= toString(rangoHumedad);
+
+    if (rangoTempResAgua < 15.55 || rangoTempResAgua > 50) {
         return false;
     }
-    else if (rangoTempTerrario < 15.55 || rangoTempTerrario > 35) {
+    else if (rangoTempTerrario < 15.55 || rangoTempTerrario > 50) {
         return false;
     }
-    else if (rangoHumedad < 15.55 || rangoHumedad > 35) {
+    else if (rangoHumedad < 15.55 || rangoHumedad > 50) {
+        return false;
+    }
+    else if (!aux1.includes(".")) {
+        return false;
+    }
+    else if (!aux2.includes(".")) {
+        return false;
+    }
+    else if (!aux3.includes(".")) {
         return false;
     }
 }
--- resources/preload.js
+++ resources/preload.js
@@ -11,14 +11,27 @@ window.addEventListener("DOMContentLoaded", () => {
 		var rangoTempTerrario= document.getElementById("TempTerrario").value;
 		var rangoHumedad= document.getElementById("Humedad").value;
 
-		if (rangoTempResAgua== null || rangoTempResAgua== "" || rangoTempResAgua < 15.55 || rangoTempResAgua > 35) {
-			ipcRenderer.send("alertaForm");
+		let aux1= toString(rangoTempResAgua);
+		let aux2= toString(rangoTempTerrario);
+		let aux3= toString(rangoHumedad);
+
+		if (rangoTempResAgua < 15.55 || rangoTempResAgua > 50) {
+			return false;
 		}
-		else if (rangoTempTerrario== null || rangoTempTerrario== "" || rangoTempTerrario < 15.55 || rangoTempTerrario > 35) {
-			ipcRenderer.send("alertaForm");
+		else if (rangoTempTerrario < 15.55 || rangoTempTerrario > 50) {
+			return false;
 		}
-		else if (rangoHumedad== null || rangoHumedad== "" || rangoHumedad < 15.55 || rangoHumedad > 35) {
-			ipcRenderer.send("alertaForm");
+		else if (rangoHumedad < 15.55 || rangoHumedad > 50) {
+			return false;
+		}
+		else if (!aux1.includes(".")) {
+			return false;
+		}
+		else if (!aux2.includes(".")) {
+			return false;
+		}
+		else if (!aux3.includes(".")) {
+			return false;
 		}
 	});
 });

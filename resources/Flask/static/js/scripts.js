7301e8272320951ce71d11881cd7ef6818b0899d
MemoOR <dethkarz2009@hotmail.com>
2021-07-27 14:35:43 -0500

    add virtual keyboard

--- resources/Flask/static/js/scripts.js
+++ resources/Flask/static/js/scripts.js
@@ -16,3 +16,18 @@ $(function () {
 		});
 	}
 });
+
+//teclado en pantalla
+$(function () {
+	$("#virtual-keyboard button").on("click", function () {
+		if ($(this).attr("data") == "DEL") {
+			board_text = $("textarea.board").text();
+			board_text = board_text.substring(0, board_text.length - 1);
+			$("textarea.board").text(board_text);
+		} else {
+			$("textarea.board").text(
+				$("textarea.board").text() + $(this).attr("data")
+			);
+		}
+	});
+});

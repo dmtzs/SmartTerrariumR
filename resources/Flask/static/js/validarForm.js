c441712f0593c882a63ba19ff5b9f9e1236a311d
Diego Ms <diego.mtzs00@gmail.com>
2021-07-28 16:04:48 -0500

    Update for js form validation

--- resources/Flask/static/js/validarForm.js
+++ resources/Flask/static/js/validarForm.js
@@ -3,15 +3,13 @@ function validarFormulario() {
 	let rangoTempTerrario= document.getElementById("TempTerrario").value;
 	let rangoHumedad= document.getElementById("Humedad").value;
 
-    //Validar en caso de que sea vacio, es decir, "". Ya que ahora si se deja vacío salta la alerta de error.
-
-    if (rangoTempResAgua < 15.55 || rangoTempResAgua > 50) {
+    if ((rangoTempResAgua < 15.55) && (rangoTempResAgua != "") || rangoTempResAgua > 50) {
         return false;
     }
-    else if (rangoTempTerrario < 15.55 || rangoTempTerrario > 50) {
+    else if ((rangoTempResAgua < 15.55) && (rangoTempResAgua != "") || rangoTempTerrario > 50) {
         return false;
     }
-    else if (rangoHumedad < 15.55 || rangoHumedad > 50) {
+    else if ((rangoTempResAgua < 15.55) && (rangoTempResAgua != "") || rangoHumedad > 50) {
         return false;
     }
 }
--- resources/preload.js
+++ resources/preload.js
@@ -11,15 +11,13 @@ window.addEventListener("DOMContentLoaded", () => {
 		var rangoTempTerrario= document.getElementById("TempTerrario").value;
 		var rangoHumedad= document.getElementById("Humedad").value;
 
-		//Validar en caso de que sea vacio, es decir, "". Ya que ahora si se deja vacío salta la alerta de error.
-
-		if (rangoTempResAgua < 15.55 || rangoTempResAgua > 50) {
+		if ((rangoTempResAgua < 15.55) && (rangoTempResAgua != "") || rangoTempResAgua > 50) {
 			ipcRenderer.send("alertaFormError");
 		}
-		else if (rangoTempTerrario < 15.55 || rangoTempTerrario > 50) {
+		else if ((rangoTempResAgua < 15.55) && (rangoTempResAgua != "") || rangoTempTerrario > 50) {
 			ipcRenderer.send("alertaFormError");
 		}
-		else if (rangoHumedad < 15.55 || rangoHumedad > 50) {
+		else if ((rangoTempResAgua < 15.55) && (rangoTempResAgua != "") || rangoHumedad > 50) {
 			ipcRenderer.send("alertaFormError");
 		}
 		else {

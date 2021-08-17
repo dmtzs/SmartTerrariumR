ad5ec1178facd8894ad307350868e9b8d06f5453
diego <diego.mtzs00@gmail.com>
2021-08-17 10:46:29 -0700

    Update InstalacionBase python file
    Added a command line functionality in order to know what does the program need to do

--- InstalacionBase.py
+++ InstalacionBase.py
@@ -1,6 +1,7 @@
 try:
     import os
     import platform
+    import sys
 except ImportError as eImp:
     print(f"The following import error ocurred: {eImp}")
 
@@ -17,12 +18,12 @@ def execComands(comandsExec):
     for comm in comandsExec:
         os.system(comm)
 
-def main(sistema):
+def installBase(sistema):
     # @Description: Variables that contains commands according to the operative system that the program is being executed
     comandosLinux= ["sudo apt update", "sudo apt upgrade", "sudo apt install python3-pip", "pip3 install -r requirements.txt", "sudo apt install nodejs",
-                    "sudo apt install npm", "npm install electron wait-port", "sudo usermod -a -G dialout $USER"]
+                    "sudo apt install npm", "npm install electron wait-port --save -prod", "sudo usermod -a -G dialout $USER"]
     
-    comandosWindows= ["pip install -r requirements.txt", "npm install electron wait-port"]
+    comandosWindows= ["pip install -r requirements.txt", "npm install electron wait-port --save -prod"]
 
     if sistema== "Windows":
         execComands(comandosWindows)
@@ -30,6 +31,33 @@ def main(sistema):
         execComands(comandosLinux)
     else:
         print("This program can be executed only in Windows and Linux operative systems")
+
+def main(sistema):
+    lenargv= len(sys.argv)
+
+    if lenargv== 2:
+        if sys.argv[1]== "--install":
+            installBase(sistema)
+        elif sys.argv[1]== "--exeFlask":
+            print("Aquí ejecutar el comando necesario para poder hacer el ejecutable del flask")
+        elif sys.argv[1]== "--help":
+            os.system("clear")
+            cadeInstall= "Para instalar todas las dependencias necesarias del programa"
+            cadeExe= "Para crear el ejecutable con pyinstaller para el back end de flask"
+            commPython= ""
+
+            if sistema== "Windows":
+                commPython= "python"
+            else:
+                commPython= "python3"
+
+            print(f"\n\n\n{commPython} InstalacionBase.py --install: {cadeInstall}")
+            print(f"\n{commPython} InstalacionBase.py --exeFlask: {cadeExe}")
+            print(f"\n{commPython} InstalacionBase.py --help: Para mostrar el presente panel de ayuda\n")
+        else:
+            print("No ingresaste ningún comando válido")
+    else:
+        print("Solo se puede recibir un argumento, no más y no menos")
 
 if __name__ == "__main__":
     try:
@@ -41,4 +69,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Se presiono Ctrl+c, finalizando programa con ejecución incorrecta")
     finally:
-        print("Finalizando ejecución de programa de instalaciones base")
+        print("Finalizando ejecución de programa")

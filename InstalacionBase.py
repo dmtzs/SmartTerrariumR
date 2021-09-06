7ff9d956c2c2bcbf52772e0b6f92de5eefb50940
Diego Ms <diego.mtzs00@gmail.com>
2021-09-05 21:32:46 -0500

    Update for InstalacionBase python file
    Change in the installation of some npm packages

--- InstalacionBase.py
+++ InstalacionBase.py
@@ -24,7 +24,7 @@ def execComands(comandsExec):
 def installBase(sistema):
     # @Description: Variables that contains commands according to the operative system that the program is being executed
     comandosLinux= ["sudo apt update", "sudo apt upgrade", "sudo apt install python3-pip", "pip3 install -r requirements.txt", "sudo apt install nodejs",
-                    "sudo apt install npm", "npm install electron wait-port --save-prod", "sudo usermod -a -G dialout $USER"]
+                    "sudo apt install npm", "npm install wait-port --save-prod", "npm install electron electron-builder --save-dev", "sudo usermod -a -G dialout $USER"]
     
     comandosWindows= ["pip install -r requirements.txt", "npm install electron wait-port --save-prod"]
 

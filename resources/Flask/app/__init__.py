4a0906eab7d05df4ec79d5c3f746d2b7210b6d5c
Diego MS <diego.mtzs00@gmail.com>
2021-10-11 20:46:41 -0500

    Refactor of flask application
    Refactor in folders, libraries and the way we program the code

---
+++ resources/Flask/app/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from flask import Flask
+except ImportError as eImp:
+    print(f"Ocurrió el error de importación: {eImp}")
+
+# Creation of the flask app.
+app = Flask(__name__)
+app.secret_key = "clave_secreta_flask"
+
+from app import routes

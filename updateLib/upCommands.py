92a4fb451bae634f09d1d4663d8f78e9539ba6a2
Diego Ms <diego.mtzs00@gmail.com>
2021-11-06 12:10:49 -0600

    Update upCommands.py
    Implementation of the methods for update server and elecctron app, I still need to look for the way to upload the arduino

--- updateLib/upCommands.py
+++ updateLib/upCommands.py
@@ -1,17 +1,48 @@
 try:
+    import os
     import sys
+    import wget
     import json
 except ImportError as eImp:
     print(f"Ocurrió el siguiente error de importación: {eImp}")
 
-class extraMethods():
+class ExtraMethods():
     def helpMessages(self):
         print("--updateArduino: Updates the arduino with the .hex file that is in the release part of the github project")
         print("--updateServer: Updates the flask server with the .exe file of the flask server that is in the release part of the github project")
         print("--updateElectron: Updates the electron .AppImage app that is in the release part of the github project")
+        print("--updateAll: Updates complete Smart terrarium app, teh server, the electron app and the arduino")
         print("--help: show this help screen")
 
-class UpdateMethods(extraMethods):
+    def jsonDataMet(self):
+        with open("resources/appData.json", "r") as jsonFile:
+            self.jsonData= json.load(jsonFile)
+
+        self.jsonData= self.jsonData["updates"]
+
+        self.arduinoLink= self.jsonData["release-arduino"]
+        self.serverLink= self.jsonData["release-server"]
+        self.electronLink= self.jsonData["release-electron"]
+
+    def updateServer(self):
+        os.remove("Server")
+        wget.download(self.serverLink)
+        print("Server updated")
+
+    def updateElectron(self):
+        os.remove("SmartTerra.AppImage")
+        wget.download(self.electronLink)
+        print("Electron app updated")
+
+    def updateArduino(self):
+        print("Aún en desarrollo")
+
+class UpdateMethods(ExtraMethods):
+    serverLink= None
+    electronLink= None
+    arduinoLink= None
+    jsonData= None
+
     def __init__(self, command, system):
         self.command= command
         self.system= system
@@ -19,18 +50,22 @@ class UpdateMethods(extraMethods):
     def coreUpdate(self):
         lenargv= len(sys.argv)
 
+        self.jsonDataMet()
+
         if lenargv== 2:
             if sys.argv[1]== "--updateArduino":
-                print("Aún en desarrollo")
+                self.updateArduino()
 
             if sys.argv[1]== "--updateServer":
-                print("Aún en desarrollo")
+                self.updateServer()
 
             if sys.argv[1]== "--updateElectron":
-                print("Aún en desarrollo")
+                self.updateElectron()
 
             if sys.argv[1]== "--updateAll":
-                print("Aún en desarrollo")
+                self.updateElectron()
+                self.updateServer()
+                self.updateArduino()
 
             if sys.argv[1]== "--help":
                 self.helpMessages()

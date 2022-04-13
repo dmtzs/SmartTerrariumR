0f5ffcee6c45af49dbfb6f34e358365297db000a
dmtzs <diego.mtzs00@gmail.com>
2022-04-13 16:21:11 -0500

    Update ArduinoConnection.py
    Final version of the library of the terrarium, we detect dinamically the serial port in which the arduino is connected

--- resources/Flask/TerrariumLib/ArduinoConnection.py
+++ resources/Flask/TerrariumLib/ArduinoConnection.py
@@ -54,14 +54,17 @@ class ArduinoConnection():
                 if len(arduino_ports) > 1:
                     warnings.warn('Multiple Arduinos found - using the first')
 
-                self.connection = serial.Serial(
-                    arduino_ports[0], self.baudrate, timeout=self.timeout)
+                self.connection = serial.Serial(arduino_ports[0], self.baudrate, timeout=self.timeout)
             else:
-                serial_port = "/dev/" + \
-                    os.popen(
-                        "dmesg | egrep ttyACM | cut -f3 -d: | tail -n1").read().strip()
-                self.connection = serial.Serial(
-                    serial_port, baudrate=self.baudrate, timeout=self.timeout)
+                arduino_ports = [
+                    p.device
+                    for p in serial.tools.list_ports.comports()
+                ]
+                if not arduino_ports:
+                    raise IOError("No Arduino found")
+                if len(arduino_ports) > 1:
+                    warnings.warn('Multiple Arduinos found - using the first')
+                self.connection = serial.Serial(arduino_ports[0], baudrate=self.baudrate, timeout=self.timeout)
             self.recieving = True
         except Exception as e:
             print(f"\n\n\t\t\t\tOcurrió el ERROR: {e}")

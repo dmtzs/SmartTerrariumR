47fd8506329d39f9b7b3cc8da8483f469e24a340
dmtzs <diego.mtzs00@gmail.com>
2022-04-13 17:34:06 -0500

    Update InstalacionBase.py
    Correction in the commands that are exxecuted in the raspberry because now with raspberry pi os are not neccesary to execute them

--- InstalacionBase.py
+++ InstalacionBase.py
@@ -53,9 +53,12 @@ def localLibs():
 # @Description: Method for install all the dependencies we need in order to make the program work in the way its supposed to be.
 def installBase(sistema):
     # @Description: Variables that contains commands according to the operative system that the program is being executed
-    comandosLinux= ["sudo apt update", "sudo apt upgrade -y", "sudo apt install python3-pip", "pip3 install -r requirements.txt", "sudo apt install curl",
+    # comandosLinux= ["sudo apt update", "sudo apt upgrade -y", "sudo apt install python3-pip", "pip3 install -r requirements.txt", "sudo apt install curl",
+    #                 "curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -", "cat /etc/apt/sources.list.d/nodesource.list", "sudo apt install -y nodejs",
+    #                 "npm install wait-port --save-prod", "npm install electron electron-builder --save-dev", "sudo usermod -a -G dialout $USER"]
+    comandosLinux= ["pip3 install -r requirements.txt", "sudo apt install curl",
                     "curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -", "cat /etc/apt/sources.list.d/nodesource.list", "sudo apt install -y nodejs",
-                    "npm install wait-port --save-prod", "npm install electron electron-builder --save-dev"]#, "sudo usermod -a -G dialout $USER"
+                    "npm install wait-port --save-prod", "npm install electron electron-builder --save-dev"]
     
     comandosWindows= ["pip install -r requirements.txt", "npm install electron wait-port --save-prod", "npm install electron electron-builder --save-dev"]
 

d3dd0a0ce98e9e75cbfc68d7c83f20499a977cc0
Diego Martínez Sánchez <diego.mtzs00@gmail.com>
2022-05-13 07:32:22 -0500

    Update initsDevMode.sh
    Added another line in which I run another script to process telegram commands

--- initsDevMode.sh
+++ initsDevMode.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 cd /home/dmtzs/Documentos
 /usr/bin/python3 "telegram1.py" "--rebootRasp"
+/usr/bin/python3 commandsTelegram.py &
 
 cd /home/dmtzs/Documentos/SmartTerrariumR
 DISPLAY=:0 npm start
+
+# pkill -xf "python3 commandsTelegram.py"

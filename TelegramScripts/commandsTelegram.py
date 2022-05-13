2546a29e258ea5736309acb969820e8d4bc7b702
dmtzs <diego.mtzs00@gmail.com>
2022-05-13 10:48:59 -0500

    Update commandsTeelgram.py
    Added a command that can be used to reboot the raspberry

--- TelegramScripts/commandsTelegram.py
+++ TelegramScripts/commandsTelegram.py
@@ -1,23 +1,26 @@
-# This is a simple echo bot using the decorator mechanism.
-# It echoes any incoming text messages.
-
-import telebot
+try:
+    import telebot
+    import subprocess
+except ImportError as eImp:
+    print(f"The following import ERROR occurred in {__file__}: {eImp}")
 
 API_TOKEN = "5397598254:AAGQtp5cBytl8J8uSEYRXTDI7-UEjRQyQik"
 
 bot = telebot.TeleBot(API_TOKEN)
 
-
-# Handle '/start' and '/help'
 # @bot.message_handler(commands=['help', 'start'])
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=["help"])
 def send_welcome(message):
-    bot.reply_to(message, "This is an alert bot to know raspberry status\nTo execute commands write in the chat an / and you will see the list of commands available")
+    message_to_send = """Este es un bot en el que puedes ejecutar acciones desde telegram, solo agregas un /comando 
+    para que puedas ejecutar uno de los comandos disponibles que salen como opciones en el canal donde el bot está 
+    instalado. De igual manera es un bot que notifica si está abajo el servidor o se a reiniciado la raspberry, etc."""
+    bot.reply_to(message, message_to_send)
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-# @bot.message_handler(func=lambda message: True)
-# def echo_message(message):
-#     bot.reply_to(message, message.text)
+@bot.message_handler(commands=["reboot"])
+def reboot_rasp(message):
+    message_to_send = "Reiniciando raspberry"
+    bot.reply_to(message, message_to_send)
+    subprocess.Popen('reboot', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 if __name__ == '__main__':
     bot.infinity_polling()

e0f7204a0e9013ae59b42372a23fc75e487e9ab2
dmtzs <diego.mtzs00@gmail.com>
2023-12-30 19:04:57 -0600

    Update main.py
    Refactor of code in main file, following good practices in python

--- resources/Flask/main.py
+++ resources/Flask/main.py
@@ -1,12 +1,21 @@
-#@File: main.py
-#@Author: Diego Martínez Sánchez and Guillermo Ortega Romo.
-#@Description: This file starts all the complete server builded with flask, this code has only the way the app should be executed.
-#              Which means if the server should be executed in development mode or production.
+"""
+This file starts all the complete server builded with flask, this code has only the way the app should be executed.
+Which means if the server should be executed in development mode or production.
+
+This file is part of SmartTerrariumRpi.
+
+SmartTerrariumRpi - A project to control a terrarium with a Raspberry Pi, for now only tested in rpi 4.
+
+This project is licensed under the MIT License. See LICENSE.md file for more information.
+
+Diego Martínez Sánchez and Guillermo Ortega Romo - 2021
+"""
 try:
+    import traceback
     from app import app
     from gevent.pywsgi import WSGIServer
-except ImportError as eImp:
-    print(f"Ocurrió el error de importación en el archivo {__file__}: {eImp}")
+except ImportError as err_imp:
+    print(f"Ocurrió el error de importación en el archivo {__file__}: {err_imp}")
 
 #-------------------------------Execute----------------------------------------#
 if __name__ == "__main__":
@@ -15,12 +24,13 @@ if __name__ == "__main__":
         # app.run(host="127.0.0.1", port=5000, debug=True, use_reloader=False)
 
         # -----------------Prod mode----------------
-        appServer=  WSGIServer(("127.0.0.1", 5000), app)
-        appServer.serve_forever()
+        app_server =  WSGIServer(("127.0.0.1", 5000), app)
+        app_server.serve_forever()
     except KeyboardInterrupt:
         print("Se presionó Ctrl + C")
         print("Apagando servidor...")
     except Exception as err:
         print(f"Ocurrió el siguiente error: {err}")
+        print(f"Error completo: \n{traceback.format_exc()}")
     else:
         print("\nReiniciando servidor")

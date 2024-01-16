8551eabb6b775b03bed4b57e17e7bed8c620893d
dmtzs <diego.mtzs00@gmail.com>
2024-01-16 09:05:54 -0600

    Update error_handlers.py
    Renamed the error handlers file

--- resources/Flask/app/error_handlers.py
+++ resources/Flask/app/error_handlers.py
@@ -1,12 +1,18 @@
 try:
     from app import app
     from flask import render_template
-except ImportError as eImp:
-    print(f"En el archivo {__file__} currió el error de importación: {eImp}")
+except ImportError as err_imp:
+    print(f"In file: {__file__} the following import error ocurred: {err_imp}")
 
 #----------------------------Error Handlers------------------------------------#
 
 # @Description: Endpoint to verify error 500 if its the case.
 @app.route("/error500")
-def error():
+def error() -> str:
+    """
+    Endpoint to verify error 500 if its the case.
+
+    Returns:
+    - str: HTML template with the error 500.
+    """
     return render_template('errorHandlers/error500.html')
